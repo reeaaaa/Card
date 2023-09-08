7977ff20f3b70a8c5d87883a9e724f33a5b7b2ab
reeaaaa <duongthienthanh110@gmail.com>
2023-09-08 15:39:14 +0700

    80

--- same_pezi.js
+++ same_pezi.js
@@ -1,12 +1,20 @@
-$('button').on('mouseover',function() {
-    $(this).css({
-      top: (Math.random()*100)+'%',
-      left: (Math.random()*100)+'%' 
+var caller = $("#emcee"),
+    box = $("#box");
+
+function foo()
+{
+    var randX = Math.floor(Math.random() * (window.innerWidth - 100));
+    var randY = Math.floor(Math.random() * (window.innerHeight - 100));
+    console.log([randX, randY]);
+    caller.stop().animate({"left": randX + "px", "top": randY + "px"});
+
+}
+
+$(document).ready(function() {
+    
+    caller.on('mouseenter', foo);
+    caller.on('click', function(){
+        alert('clicked!');
     });
-  });
-$('button').on('mouseclick',function() {
-  $(this).css({
-    top: (Math.random()*100)+'%',
-    left: (Math.random()*100)+'%' 
-  });
+    
 });
