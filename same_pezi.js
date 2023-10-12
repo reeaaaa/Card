70ac2d12855c49973ff503830d7b3a7657037e8a
Duong Thien Thanh <139046298+reeaaaa@users.noreply.github.com>
2023-10-12 10:14:32 +0700

    Update same_pezi.js

--- same_pezi.js
+++ same_pezi.js
@@ -1,13 +1,1 @@
-$('button').on('mouseover',function() {
-    $(this).css({
-      top: (Math.random()*100)+'%',
-      left: (Math.random()*100)+'%' 
-    });
-  });
-  
-  $('button').on('mouseclick',function() {
-    $(this).css({
-      top: (Math.random()*100)+'%',
-      left: (Math.random()*100)+'%' 
-    });
-  });
+
